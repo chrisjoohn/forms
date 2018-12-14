ba139c34e03658b8e417bb0a2fe7780af8f17c54
Chris John Mulingbayan <chrisjohnmulingbayan@gmail.com>
2018-12-14 13:30:59 +0800

    Already fixed the bugs and is ready to be checked.

--- action_page.php
+++ action_page.php
@@ -1,8 +1,52 @@
-<html>
+<!DOCTYPE html>
+<html lang='en'>
+    <head>
+        <meta charset='utf-8'/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <link rel="stylesheet" href="action_page.css">
+    </head>
     <body>
-        Name: <?php echo $_GET['fname'] . " " . $_GET['lname']; ?><br/>
-        Address: <?php echo $_GET['address'];?> <br/>
-        Contact: <br/> <?php echo "Phone number: " . $_GET['phone'];?><br/>
-        <?php echo "Mobile number: " . $_GET['mobile'];?>
+        <h2 class="heading">
+            Checkout Form
+        </h2>
+        <div class="body-container">
+            <div class="date-container">
+                <p class="upper-right">
+                    Date: <?php echo $_GET['date'];?><br/>
+                    Order Number: <?php echo $_GET['orderNumber']; ?>
+                </p>
+            </div>
+            <div class="info-container">
+                <h3 class="info-header">
+                    Customer Information
+                </h3>
+                <p class="information">
+                    Customer Name: <?php echo $_GET['fname'] ." ". $_GET['lname'];?> <br/>
+                    Address: <?php echo $_GET['address'];?><br/>
+                    Phone number: <?php echo $_GET['phone'];?><br/>
+                    Mobile number: <?php echo $_GET['mobile'];?><br/>
+                    Email Address: <?php echo $_GET['email'];?><br/>
+                </p>
+            </div>
+            <div class="table-container">
+                <h3>
+                    Your Orders:
+                </h3>
+                <table>
+                    <tr>
+                        <th>Description</th>
+                        <th>Quantity</th>
+                        <th>Price</th>
+                        <th>Total</th>
+                    </tr>
+                    <tr>
+                        <td><?php $_GET['desc1'];?></td>
+                        <td><?php $_GET['quantity1'];?></td>
+                        <td><?php $_GET['uprice1'];?></td>
+                        <td><?php $_GET['trpice1'];?></td>
+                    </tr>
+                </table>
+            </div>
+        </div>        
     </body>
 </html>
--- forms.js
+++ forms.js
@@ -3,59 +3,68 @@ var date = currentDate.getDate();
 var month = currentDate.getMonth();
 var year = currentDate.getFullYear();
 var monthDateYear  = (month+1) + "/" + date + "/" + year;
-
 document.getElementById('date').value = monthDateYear;
 
 
 
 var ID = function(){
-    return 'id_' + currentDate.getTime();
+    return 'ID_' + currentDate.getTime();
 }
 
 document.getElementById('orderNumber').value = ID();
 
-
-var q1, q2, q3, q4, q5, u1, u2, u3, u4, u5, t1, t2, t3, t4, t5, total;
 /*QUANTITY*/
 
-q1 = document.getElementById('quantity1').value;
-q2 = document.getElementById('quantity2').value;
-q3 = document.getElementById('quantity3').value;
-q4 = document.getElementById('quantity4').value;
-q5 = document.getElementById('quantity5').value;
+var u1, u2, u3, u4, u5, t1, t2, t3, t4, t5;
+var q1, q2, q3, q4, q5, total = 0;
 
 
-/*UNIT PRICE*/
-
-u1 = document.getElementById('uprice1').value;
-u2 = document.getElementById('uprice2').value;
-u3 = document.getElementById('uprice3').value;
-u4 = document.getElementById('uprice4').value;
-u5 = document.getElementById('uprice5').value;
-
-/*TOTAL PRICE */
-
-t1 = q1 * u1;
-t2 = q2 * u2;
-t3 = q3 * u3;
-t4 = q4 * u4;
-t5 = q5 * u5;
-
-document.getElementById('tprice1').value =  t1;
-document.getElementById('tprice2').value = t2;
-document.getElementById('tprice3').value = t3;
-document.getElementById('tprice4').value = t4;
-document.getElementById('tprice5').value = t5;
-
-
-/*
-
-total = [t1, t2, t3, t4, t5];
-for(var i = 0; i <= total.length; i++){
-    total += total[i];
-    return total;
+function changeValue1(val){
+    q1 = (document.getElementById('quantity1').value = val);
+    q1 = Number(q1);
+    u1 = document.getElementById('uprice1').value;
+    t1 = q1 * u1;
+    document.getElementById('tprice1').value = t1;
+    total = t1 + total;
+    document.getElementById('total').value = total;
 }
 
-document.getElementById('total').value = &#x20B1 + total;
+function changeValue2(val){
+    q2 = (document.getElementById('quantity2').value = val);
+    q2 = Number(q2)
+    u2 = document.getElementById('uprice2').value;
+    t2 = q2 * u2;
+    document.getElementById('tprice2').value = t2;
+    total = t2 + total;
+    document.getElementById('total').value = total;
+}
 
-*/
+function changeValue3(val){
+    q3 = (document.getElementById('quantity3').value = val);
+    q3 = Number(q3)
+    u3 = document.getElementById('uprice3').value;
+    t3 = q3 * u3;
+    document.getElementById('tprice3').value = t3;
+    total = total + t3;
+    document.getElementById('total').value = total;
+}
+
+function changeValue4(val){
+    q4 = (document.getElementById('quantity4').value = val);
+    q4 = Number(q4)
+    u4 = document.getElementById('uprice4').value;
+    t4 = q4 * u4;
+    document.getElementById('tprice4').value = t4;
+    total = total + t4;
+    document.getElementById('total').value = total;
+}
+
+function changeValue5(val){
+    q5 = (document.getElementById('quantity5').value = val);
+    q5 = Number(q5)
+    u5 = document.getElementById('uprice5').value;
+    t5 = q5 * u5;
+    document.getElementById('tprice5').value = t5;
+    total = total + t5;
+    document.getElementById('total').value = total;
+}
